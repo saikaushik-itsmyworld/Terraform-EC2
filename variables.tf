3bc9bc1577e077300acf2f5aba7b09f0d4073293
saimudiki <mudiki08@gmail.com>
2019-03-24 00:20:22 -0400

    security_group_var

--- variables.tf
+++ variables.tf
@@ -9,3 +9,22 @@ variable "region" {
 
 default ="us-east-1"
 }
+variable ingress {
+      from_port = 0
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+variable  egress {
+      from_port = 0
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #variable instance_count {
+    #  default = 3
+ # }
+ output "public_ips" {
+  value = ["${aws_instance.example.*.public_ip}"]
+}
