2cd8bad6d75c78b476c5b7280cb4b857676a61de
saimudiki <mudiki08@gmail.com>
2019-03-24 00:18:20 -0400

    security_groupv

--- ec2.tf
+++ ec2.tf
@@ -4,28 +4,21 @@ secret_key= "${var.secret_key}"
 region="${var.region}"
 version = "~> 1.7"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
     ami = "ami-0a313d6098716f372"
     instance_type = "t2.micro"
+    count = 4
     key_name = "AWS EC2 Key Pair"
+
      tags {
-    Name = "test-instance"
+    Name = "test-${count.index}"
   }
 }
 resource "aws_security_group" "allowall" {
   name ="allowall"
   description="Allow all Inbound and outbound traffic"
 
-  ingress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = "${var.ingress}"
+
+  egress = "${var.egress}"
 }
